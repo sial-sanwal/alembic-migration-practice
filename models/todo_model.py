a0e67a97eb5ad8b83c96faca20eab2e262edf271
sial-sanwal <sialsanwal885@gmail.com>
2025-03-03 16:21:08 +0500

    add the student table in db

--- models/todo_model.py
+++ models/todo_model.py
@@ -10,3 +10,9 @@ class todo(Base):
     discription=Column(String,nullable=True)
     completed=Column(Boolean,default=False)
     task_done=Column(Boolean,default=False)
+    
+    __tablename__='student'
+    id=Column(Integer,primary_key=True,index=True)
+    Name=Column(String,index=True)
+    Standard=Column(String,nullable=True)
+    completed=Column(Boolean,default=False)
